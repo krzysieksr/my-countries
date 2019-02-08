192564b804add9306af7000f411a2672912823e2
Krzysiek <krzysieksr@wp.pl>
2019-02-08 16:00:12 +0100

    Modified country query.

--- src/main/java/com/country/dao/CountryDAOImpl.java
+++ src/main/java/com/country/dao/CountryDAOImpl.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CountryDAOImpl implements CountryDAO {
@@ -16,6 +17,13 @@ public class CountryDAOImpl implements CountryDAO {
     private static final String SELECT_ALL_COUNTRIES = "SELECT * FROM country";
     private static final String GET_COUNTRIES_SORTED_BY_AREA = "SELECT * FROM country ORDER BY area DESC";
     private static final String GET_COUNTRIES_SORTED_BY_POPULATION = "SELECT * FROM country ORDER BY population DESC";
+
+    private static final String GET_LANGUAGES_FOR_COUNTRY =
+            "SELECT l.* FROM lang l INNER JOIN country_lang cl ON cl.lang_id=l.lang_id WHERE cl.country_id=";
+
+    private static final String GET_RELIGIONS_FOR_COUNTRY =
+            "SELECT r.religionName FROM religion r INNER JOIN country_religion cr ON cr.religion_id=r.religion_id WHERE cr.country_id=";
+
 
     @Override
     public List<CountryProtos.Country> getCountries() throws SQLException {
@@ -75,7 +83,8 @@ public class CountryDAOImpl implements CountryDAO {
         List<CountryProtos.Country> countryList = new ArrayList<>();
         ResultSet resultSet = statement.executeQuery();
         while (resultSet.next()) {
-            CountryProtos.Country country = CountryProtos.Country.newBuilder()
+
+            CountryProtos.Country.Builder builder = CountryProtos.Country.newBuilder()
                     .setName(resultSet.getString("name"))
                     .setPopulation(resultSet.getInt("population"))
                     .setArea(resultSet.getInt("area"))
@@ -83,8 +92,42 @@ public class CountryDAOImpl implements CountryDAO {
                     .setGdp(resultSet.getInt("GDP"))
                     .setGovernmentType(resultSet.getString("government_type"))
                     .setCurrency(resultSet.getString("currency"))
-                    .setInternetDomain(resultSet.getString("internet_domain"))
-                    .build();
+                    .setInternetDomain(resultSet.getString("internet_domain"));
+
+
+            int country_id = resultSet.getInt("country_id");
+
+            DataSource pool = ConnectionPoolContextListener.dataSource;
+            try (Connection conn = pool.getConnection()) {
+                PreparedStatement religionStatement = conn.prepareStatement(GET_RELIGIONS_FOR_COUNTRY + country_id);
+                ResultSet religionSet = religionStatement.executeQuery();
+
+
+                List<CountryProtos.Country.Religion> religions = new LinkedList<>();
+                while (religionSet.next()) {
+                    String string = religionSet.getString("religionName");
+                    CountryProtos.Country.Religion religion = CountryProtos.Country.Religion.valueOf(string);
+                    religions.add(religion);
+                }
+
+                builder.addAllReligions(religions);
+
+
+                PreparedStatement languageStatement = conn.prepareStatement(GET_LANGUAGES_FOR_COUNTRY + country_id);
+                ResultSet languageSet = languageStatement.executeQuery();
+
+                List<CountryProtos.Country.Language> languages= new LinkedList<>();
+
+                while (languageSet.next()) {
+                    String string = languageSet.getString("langName");
+                    CountryProtos.Country.Language language = CountryProtos.Country.Language.valueOf(string);
+                    languages.add(language);
+                }
+
+                builder.addAllLanguages(languages);
+
+            }
+            CountryProtos.Country country = builder.build();
             countryList.add(country);
         }
         return countryList;
